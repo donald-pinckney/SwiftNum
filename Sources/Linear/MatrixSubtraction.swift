7e09c919cebeb45c59283ce75120f202ef11679b
Donald Pinckney <donald_pinckney@icloud.com>
2017-01-01 14:01:46 -0800

    Implement Matrix subtraction.

--- Sources/Linear/MatrixSubtraction.swift
+++ Sources/Linear/MatrixSubtraction.swift
@@ -6,4 +6,36 @@
 //
 //
 
-import Foundation
+import Accelerate
+
+public extension Matrix {
+    static func -(lhs: Matrix, rhs: Matrix) -> Matrix {
+        precondition(lhs.width == rhs.width)
+        precondition(lhs.height == rhs.height)
+        
+        var res = lhs
+        
+        vDSP_vsubD(rhs.data, 1, lhs.data, 1, &res.data, 1, vDSP_Length(lhs.width * rhs.width))
+        
+        return res
+    }
+    static func -=(lhs: inout Matrix, rhs: Matrix) {
+        precondition(lhs.width == rhs.width)
+        precondition(lhs.height == rhs.height)
+        
+        vDSP_vsubD(rhs.data, 1, lhs.data, 1, &lhs.data, 1, vDSP_Length(lhs.width * rhs.width))
+    }
+    
+    
+    static func -(lhs: Matrix, rhs: Double) -> Matrix {
+        var res = lhs
+        var rhs = -rhs
+        vDSP_vsaddD(res.data, 1, &rhs, &res.data, 1, vDSP_Length(lhs.data.count))
+        return res
+    }
+    
+    static func -=(lhs: inout Matrix, rhs: Double) {
+        var rhs = -rhs
+        vDSP_vsaddD(lhs.data, 1, &rhs, &lhs.data, 1, vDSP_Length(lhs.data.count))
+    }
+}
